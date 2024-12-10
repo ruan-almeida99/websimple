a7c7ceadf90a3a2543a84a01a90523bc207b3b41
ruan <ruan.social99@gmail.com>
2024-12-10 13:21:28 -0300

    preparando merge

--- hello_world.go
+++ hello_world.go
@@ -6,4 +6,6 @@ func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Olá bem vindo ao mundo GO!")
 	fmt.Println("Agora estou na branch dev e nao na main!")
+	fmt.Println("Agora vou realizar o merge da dev na main!")
+	fmt.Println("Estou na main!")
 }
